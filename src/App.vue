958c12c6d42d7c8ce4cbc298e73cfffcea1c4b0a
james yates <yates_7@hotmail.com>
2023-11-01 01:32:23 +0000

    push todo to array method

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,32 @@ const input_category = ref(null)
 const todos_asc = computed(() => todos.value.sort((a, b) => {
   return a.createdAt - b.createdAt
 }))
+
+const addTodo = () => {
+  if (input_content.value.trim() === '' || input_category.value === null) {
+    return
+  }
+
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: new Date().getTime()
+  })
+}
+
+watch(name, (newVal) => {
+    localStorage.setItem('todos', JSON.stringify(newVal))
+}, { deep: true })
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || ''
+})
+
 </script>
 
 <template>
@@ -20,6 +46,46 @@ const todos_asc = computed(() => todos.value.sort((a, b) => {
         <h2 class="title">
           What's up, <input type="text" placeholder="Name here" v-model="name" />
         </h2>
+      </section>
+
+      <section class="create-todo">
+          <h3>CREATE A TODO</h3>
+
+          <form @submit.prevent="addTodo">
+              <h4>What's on your todo list?</h4>
+              <input 
+                type="text" 
+                placeholder="e.g. plant a tree" 
+                v-model="input_content" />
+          
+                <h4>Pick a category</h4>
+
+                <div class="options">
+
+                  <label>
+                    <input
+                      type="radio"
+                      name="category"
+                      value="business"
+                      v-model="input_category" />
+                    <span class="bubble business"></span>
+                    <div>Business</div>
+                  </label>
+
+                  <label>
+                    <input
+                      type="radio"
+                      name="category"
+                      value="personal"
+                      v-model="input_category" />
+                    <span class="bubble personal"></span>
+                    <div>Personal</div>
+                  </label>
+
+                </div>
+
+                <input type="submit" value="Add todo"/>
+          </form>
       </section>
 
     </main>
